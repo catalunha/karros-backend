13ef2ab6f3cbdae29fa9eb007449be4ca935a68f
catalunha <catalunha.mj@gmail.com>
2024-08-05 09:00:07 -0300

    adminok

--- backend/stores/admin.py
+++ backend/stores/admin.py
@@ -15,3 +15,25 @@ class VehicleAdmin(admin.ModelAdmin):
         "year",
         "is_sold",
     ]
+    exclude = ["user"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    # def has_change_permission(self, request, obj=None):
+    #     if not obj:
+    #         return True  # So they can see the change list page
+    #     if request.user.is_superuser or obj.user == request.user:
+    #         return True
+    #     else:
+    #         return False
+
+    # has_delete_permission = has_change_permission
